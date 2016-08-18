40b0c4466a167cbe568b77140b937fea86a0c30e
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-08-18 21:46:11 +0530

    ready for submission

--- assgn7/second_final.v
+++ assgn7/second_final.v
@@ -1,4 +1,4 @@
-module half_full_adder(
+module half_adder(
     input a,
     input b,
     output sum,
@@ -16,8 +16,8 @@ module full_adder(a,b,Cin,sum, Cout);
 	output sum,Cout;
 	wire c1,c2,s;
 	
-	half_full_adder h1(a,b,s,c1);
-	half_full_adder h2(s,Cin,sum,c2);
+	half_adder h1(a,b,s,c1);
+	half_adder h2(s,Cin,sum,c2);
 	or o1(Cout,c1,c2);
 endmodule
 
@@ -35,10 +35,7 @@ generate
       not n(na[k],a[k]);
     end
   endgenerate
-  
-full_adder8 add(na,1,b,c);
-
-
+adder8 add(na,1,b,c);
 endmodule
 
 
@@ -51,52 +48,44 @@ module count_check(rout,out);
 endmodule
 
 
-module count_decr( in,out
-    );
+module decrement_count( in,out);
 input [2:0] in ;
 output [2:0]out;
 wire carry;
-full_adder8 pp({5'b 00000,in},8'b 11111111 ,out,carry);
-	
+adder8 pp({5'b 00000,in},8'b 11111111 ,out,carry);
 endmodule
 
-
-
-module load_sel(state,ld,sel
-    );
+module load_sel(state,ld,sel);
 input [2:0] state;
 output [4:0] ld;
 output [4:0] sel;
+wire [2:0] notstate;
 
-wire [2:0] nstate;
-not n0(nstate[0],state[0]);
-not n1(nstate[1],state[1]);
-not n2(nstate[2],state[2]);
+not n0(notstate[0],state[0]);
+not n1(notstate[1],state[1]);
+not n2(notstate[2],state[2]);
 
 
-and a1(ld[0],nstate[1],state[0]);
-and a2(ld[1],nstate[2],nstate[1],nstate[0]);
-and a3(w1,nstate[2],state[0]);
-and a4(w2,state[2],nstate[1],nstate[0]);
+and a1(ld[0],notstate[1],state[0]);
+and a2(ld[1],notstate[2],notstate[1],notstate[0]);
+and a3(w1,notstate[2],state[0]);
+and a4(w2,state[2],notstate[1],notstate[0]);
 or or1(ld[2],w1,w2);
-and a5(w3,nstate[2],state[1],nstate[0]);
+and a5(w3,notstate[2],state[1],notstate[0]);
 or or2(ld[3],w3,w2);
-and a6(w4,nstate[2],nstate[1],state[0]);
+and a6(w4,notstate[2],notstate[1],state[0]);
 or or3(ld[4],w4,w2);
 
-
-and aa1( sel[0],state[2],nstate[1],state[0]);
-and aa2(sel[1],state[2],nstate[1],nstate[0]);
-and aa3(ww1,nstate[2],nstate[1],state[0]);
+and aa1( sel[0],state[2],notstate[1],state[0]);
+and aa2(sel[1],state[2],notstate[1],notstate[0]);
+and aa3(ww1,notstate[2],notstate[1],state[0]);
 not nn(sel[2],ww1);
 not nn2(sel[4],ww1);
-and aa4(sel[3],state[2],nstate[1],nstate[0]);
+and aa4(sel[3],state[2],notstate[1],notstate[0]);
 endmodule
 
 
-
-
-module mux2_1(out,a,b,sel);  
+module mux2(out,a,b,sel);  
 output  out;  
 input  a,b,sel;  
   not   not1(sel_, sel); 
@@ -106,57 +95,54 @@ input  a,b,sel;
   endmodule 
 
 
-   module mux4_1(
+module mux4(
     output out,
     input [3:0] a,
-    input [1:0] sel
-    );
+    input [1:0] sel);
 
-wire [1:0] nsel;
+wire [1:0] notsel;
 wire [7:0] w;
-not n0(nsel[0],sel[0]);
-not n1(nsel[1],sel[1]);
+not n0(notsel[0],sel[0]);
+not n1(notsel[1],sel[1]);
 
-and a0(w[0],nsel[1],nsel[0],a[0]);
-and a1(w[1],nsel[1],sel[0],a[1]);
-and a2(w[2],sel[1],nsel[0],a[2]);
+and a0(w[0],notsel[1],notsel[0],a[0]);
+and a1(w[1],notsel[1],sel[0],a[1]);
+and a2(w[2],sel[1],notsel[0],a[2]);
 and a3(w[3],sel[1],sel[0],a[3]);
 or o1(out,w[0],w[1],w[2],w[3]);
 endmodule
 
 
-
-module mux8_1(
+module mux8(
     input [7:0] a,
     input [2:0] sel,
-    output out
-    );
+    output out);	 
 	 
-wire [2:0] nsel;
+wire [2:0] notsel;
 wire [7:0] w;
-not n0(nsel[0],sel[0]);
-not n1(nsel[1],sel[1]);
-not n2(nsel[2],sel[2]);
+not n0(notsel[0],sel[0]);
+not n1(notsel[1],sel[1]);
+not n2(notsel[2],sel[2]);
 
-and a0(w[0],nsel[2],nsel[1],nsel[0],a[0]);
-and a1(w[1],nsel[2],nsel[1],sel[0],a[1]);
-and a2(w[2],nsel[2],sel[1],nsel[0],a[2]);
-and a3(w[3],nsel[2],sel[1],sel[0],a[3]);
-and a4(w[4],sel[2],nsel[1],nsel[0],a[4]);
-and a5(w[5],sel[2],nsel[1],sel[0],a[5]);
-and a6(w[6],sel[2],sel[1],nsel[0],a[6]);
+and a0(w[0],notsel[2],notsel[1],notsel[0],a[0]);
+and a1(w[1],notsel[2],notsel[1],sel[0],a[1]);
+and a2(w[2],notsel[2],sel[1],notsel[0],a[2]);
+and a3(w[3],notsel[2],sel[1],sel[0],a[3]);
+and a4(w[4],sel[2],notsel[1],notsel[0],a[4]);
+and a5(w[5],sel[2],notsel[1],sel[0],a[5]);
+and a6(w[6],sel[2],sel[1],notsel[0],a[6]);
 and a7(w[7],sel[2],sel[1],sel[0],a[7]);
 or o1(out,w[0],w[1],w[2],w[3],w[4],w[5],w[6],w[7]);
 endmodule
 
 
-module full_adder8(a,b,sum,carry);
-	
+module adder8(a,b,sum,Cout);
 	input [7:0] a;
 	input [7:0] b;
 	wire [6:0]s;
 	output [7:0] sum;
-	output carry;
+	output Cout;
+	
 	full_adder u0 (a[0],b[0],1'b0,sum[0],s[0]);
 	full_adder u1 (a[1],b[1],s[0],sum[1],s[1]);
 	full_adder u2 (a[2],b[2],s[1],sum[2],s[2]);
@@ -164,7 +150,7 @@ module full_adder8(a,b,sum,carry);
 	full_adder u4 (a[4],b[4],s[3],sum[4],s[4]);
 	full_adder u5 (a[5],b[5],s[4],sum[5],s[5]);
 	full_adder u6 (a[6],b[6],s[5],sum[6],s[6]);
-	full_adder u7 (a[7],b[7],s[6],sum[7],carry);
+	full_adder u7 (a[7],b[7],s[6],sum[7],Cout);
 
 endmodule
 
@@ -172,44 +158,36 @@ endmodule
 module next_state(
     input [2:0] state,
     input flag,
-    input go,
-    output [2:0] nextstate
-    );
+    input Go,
+    output [2:0] nextstate);
 
-wire [2:0] nstate;
-not n0(nstate[0],state[0]);
-not n1(nstate[1],state[1]);
-not n2(nstate[2],state[2]);
-not n3(ngo,go);
+wire [2:0] notstate;
+not n0(notstate[0],state[0]);
+not n1(notstate[1],state[1]);
+not n2(notstate[2],state[2]);
+not n3(ngo,Go);
 not n4(nflag,flag);
 
-
-
-
 and a1(w1,state[1],state[0]);
-and a2(w2,state[2],nstate[1],nstate[0]);
-and a3(w3,state[2],nstate[1],flag);
-and a4(w4,state[2],nstate[0],go);
+and a2(w2,state[2],notstate[1],notstate[0]);
+and a3(w3,state[2],notstate[1],flag);
+and a4(w4,state[2],notstate[0],Go);
 
 or o1(nextstate[2],w1,w2,w3,w4);
 
-
 and aa1(ww1,state[2],state[0]);
-and aa2(ww2,nstate[1],state[0],ngo);
-and aa3(ww3,nstate[2],state[1],nstate[0]);
-and aa4(ww4,go,nstate[0],state[1]);
+and aa2(ww2,notstate[1],state[0],ngo);
+and aa3(ww3,notstate[2],state[1],notstate[0]);
+and aa4(ww4,Go,notstate[0],state[1]);
 
 or o2(nextstate[1],ww1,ww2,ww3,ww4);
 
-
-and ab1(wb1,nstate[2],nstate[1],go);
-and ab2(wb2,nstate[2],nstate[0],go);
-and ab3(wb3,state[2],nstate[0],ngo);
-and ab4(wb4,state[2],nstate[1],nflag);
+and ab1(wb1,notstate[2],notstate[1],Go);
+and ab2(wb2,notstate[2],notstate[0],Go);
+and ab3(wb3,state[2],notstate[0],ngo);
+and ab4(wb4,state[2],notstate[1],nflag);
 
 or o3(nextstate[0],wb1,wb2,wb3,wb4);
-
-
 endmodule
 
 
@@ -223,11 +201,8 @@ module register(in, out, clk ,load, rst);
  	begin
  	if (rst) out <= 0;
  	else if(load) out <= in;
- 
-
- 	end
+  	end
 endmodule
-
 
 
 module register_n(in, out, clk,rst);
@@ -240,15 +215,10 @@ module register_n(in, out, clk,rst);
  	begin
  	if (rst) out <= 0;
    else  out <= in;
- 
-
  	end
 endmodule
 
-
-
-module shr_2( in ,inadd ,out,res
-    );
+module shiftRightby2( in ,inadd ,out,res);
 input [7:0] in ;
 input [1:0] inadd ;
 output [7:0] out ;
@@ -258,7 +228,7 @@ assign res[0] = in[0];
 
 generate
 	 	genvar m;
-      for(m=0;m<6;m=m+1)  begin:asdas
+      for(m=0;m<6;m=m+1)  begin:loop2
           assign out[m] = in[m+2];
 		end
  assign out[6] = inadd[0];
@@ -266,11 +236,7 @@ generate
 endgenerate
 endmodule
 
-
-
-
-
-module datapath(ld,sel,clk,sw,rst,flag,display);
+module datapath(ld,sel,clk,sw,rst,flag,result);
 
 input [4:0]ld;
 input [4:0]sel;
@@ -280,134 +246,118 @@ wire[2:0]  ct ;
 wire[2:0]  ctnew;  
 wire wc3,wc2,wc1,wc0;
 output flag ;
-output [15:0] display ;
-//output [7:0]z;
-//output [7:0]y;
-//output [7:0] acc;
-wire [2:0] mainsel;
+output [15:0] result ;
 
-mux2_1 mc2( wc2,ctnew[2],1'b 1,sel[0]);
-mux2_1 mc1( wc1,ctnew[1],1'b 0,sel[0]);
-mux2_1 mc0( wc0,ctnew[0],1'b 0,sel[0]);
+wire [2:0] select;
+
+mux2 m2( wc2,ctnew[2],1'b 1,sel[0]);
+mux2 m1( wc1,ctnew[1],1'b 0,sel[0]);
+mux2 m0( wc0,ctnew[0],1'b 0,sel[0]);
 
 //in, out, clk ,load, rst
 register cnt({wc2,wc1,wc0} , ct , clk , ld[0] , rst);
-count_decr dec1(ct,ctnew) ;
+decrement_count dec1(ct,ctnew) ;
 count_check ck ( ct , flag );
-
 
 wire [7:0] md;
 wire [7:0] nmd;
 wire [7:0] c2md;
 
-
-register multiplicand(sw,md,clk,ld[1] , rst);
+register Mcandreg(sw,md,clk,ld[1] , rst);
 comp2s c1 (md,nmd);
 register compmd(nmd,c2md,clk,1'b 1 , rst);
 
-
 wire [7:0]z ;
-wire [7:0]newacc ;
+wire [7:0]nAcc ;
 wire [7:0]acc ;
-wire [7:0]macc ;
+wire [7:0]mAcc ;
 wire [7:0]mmp ;
-wire [7:0]mp;
-wire [7:0]newmp;
+wire [7:0]Mpand;
+wire [7:0]newMpand;
 wire [1:0]transshr ;
-wire [1:0]lasttwo ;
+wire [1:0]end2 ;
+
+mux4 muxacc7 (mAcc[7] ,{nAcc[7], z[7],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc6 (mAcc[6] ,{nAcc[6], z[6],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc5 (mAcc[5] ,{nAcc[5], z[5],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc4 (mAcc[4] ,{nAcc[4], z[4],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc3 (mAcc[3] ,{nAcc[3], z[3],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc2 (mAcc[2] ,{nAcc[2], z[2],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc1 (mAcc[1] ,{nAcc[1], z[1],1'b 0,1'b 0}, sel[2:1]);
+mux4 muxacc0 (mAcc[0] ,{nAcc[0], z[0],1'b 0,1'b 0}, sel[2:1]);
+register rAcc(mAcc , acc , clk , ld[2] , rst);
 
 
-mux4_1 muxacc7 (macc[7] ,{newacc[7], z[7],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc6 (macc[6] ,{newacc[6], z[6],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc5 (macc[5] ,{newacc[5], z[5],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc4 (macc[4] ,{newacc[4], z[4],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc3 (macc[3] ,{newacc[3], z[3],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc2 (macc[2] ,{newacc[2], z[2],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc1 (macc[1] ,{newacc[1], z[1],1'b 0,1'b 0}, sel[2:1]);
-mux4_1 muxacc0 (macc[0] ,{newacc[0], z[0],1'b 0,1'b 0}, sel[2:1]);
-register regacc(macc , acc , clk , ld[2] , rst);
+mux2 muxMpand7 (mmp[7] ,newMpand[7], sw[7], sel[3]);
+mux2 muxMpand6 (mmp[6] ,newMpand[6], sw[6], sel[3]);
+mux2 muxMpand5 (mmp[5] ,newMpand[5], sw[5], sel[3]);
+mux2 muxMpand4 (mmp[4] ,newMpand[4], sw[4], sel[3]);
+mux2 muxMpand3 (mmp[3] ,newMpand[3], sw[3], sel[3]);
+mux2 muxMpand2 (mmp[2] ,newMpand[2], sw[2], sel[3]);
+mux2 muxMpand1 (mmp[1] ,newMpand[1], sw[1], sel[3]);
+mux2 muxMpand0 (mmp[0] ,newMpand[0], sw[0], sel[3]);
+register regMpand(mmp , Mpand , clk , ld[3] , rst);
 
-
-mux2_1 muxmp7 (mmp[7] ,newmp[7], sw[7], sel[3]);
-mux2_1 muxmp6 (mmp[6] ,newmp[6], sw[6], sel[3]);
-mux2_1 muxmp5 (mmp[5] ,newmp[5], sw[5], sel[3]);
-mux2_1 muxmp4 (mmp[4] ,newmp[4], sw[4], sel[3]);
-mux2_1 muxmp3 (mmp[3] ,newmp[3], sw[3], sel[3]);
-mux2_1 muxmp2 (mmp[2] ,newmp[2], sw[2], sel[3]);
-mux2_1 muxmp1 (mmp[1] ,newmp[1], sw[1], sel[3]);
-mux2_1 muxmp0 (mmp[0] ,newmp[0], sw[0], sel[3]);
-register regmp(mmp , mp , clk , ld[3] , rst);
-
-
-shr_2 shracc(acc,{acc[7],acc[7]},newacc , transshr);
-shr_2 shrmp(mp,transshr,newmp , lasttwo);
-
+shiftRightby2 shracc(acc,{acc[7],acc[7]},nAcc , transshr);
+shiftRightby2 shrMpand(Mpand,transshr,newMpand , end2);
 
 wire [7:0]y;
 wire carry;
 wire lb;
 wire lastbit ; 
 
-mux2_1 reglbit(lb,lasttwo[1],1'b 0 , sel[4]);
+mux2 reglbit(lb,end2[1],1'b 0 , sel[4]);
 register #(1) lbit(lb,lastbit,clk,ld[4],rst);
 
-assign mainsel = {mp[1],mp[0],lastbit};
+assign select = {Mpand[1],Mpand[0],lastbit};
 
-mux8_1 mainmux0({1'b 0 , c2md[0],c2md[0] ,1'b0 , 1'b 0 , md[0] , md[0], 1'b0 },mainsel,y[0] );
-mux8_1 mainmux1({1'b 0 , c2md[1],c2md[1] ,c2md[0] , md[0] , md[1] , md[1], 1'b0 },mainsel,y[1] );
-mux8_1 mainmux2({1'b 0 , c2md[2],c2md[2] ,c2md[1] , md[1] , md[2] , md[2], 1'b0 },mainsel,y[2] );
-mux8_1 mainmux3({1'b 0 , c2md[3],c2md[3] ,c2md[2] , md[2] , md[3] , md[3], 1'b0 },mainsel,y[3] );
-mux8_1 mainmux4({1'b 0 , c2md[4],c2md[4] ,c2md[3] , md[3] , md[4] , md[4], 1'b0 },mainsel,y[4] );
-mux8_1 mainmux5({1'b 0 , c2md[5],c2md[5] ,c2md[4] , md[4] , md[5] , md[5], 1'b0 },mainsel,y[5] );
-mux8_1 mainmux6({1'b 0 , c2md[6],c2md[6] ,c2md[5] , md[5] , md[6] , md[6], 1'b0 },mainsel,y[6] );
-mux8_1 mainmux7({1'b 0 , c2md[7],c2md[7] ,c2md[6] , md[6] , md[7] , md[7], 1'b0 },mainsel,y[7] );
+mux8 outMux0({1'b 0 , c2md[0],c2md[0] ,1'b0 , 1'b 0 , md[0] , md[0], 1'b0 },select,y[0] );
+mux8 outMux1({1'b 0 , c2md[1],c2md[1] ,c2md[0] , md[0] , md[1] , md[1], 1'b0 },select,y[1] );
+mux8 outMux2({1'b 0 , c2md[2],c2md[2] ,c2md[1] , md[1] , md[2] , md[2], 1'b0 },select,y[2] );
+mux8 outMux3({1'b 0 , c2md[3],c2md[3] ,c2md[2] , md[2] , md[3] , md[3], 1'b0 },select,y[3] );
+mux8 outMux4({1'b 0 , c2md[4],c2md[4] ,c2md[3] , md[3] , md[4] , md[4], 1'b0 },select,y[4] );
+mux8 outMux5({1'b 0 , c2md[5],c2md[5] ,c2md[4] , md[4] , md[5] , md[5], 1'b0 },select,y[5] );
+mux8 outMux6({1'b 0 , c2md[6],c2md[6] ,c2md[5] , md[5] , md[6] , md[6], 1'b0 },select,y[6] );
+mux8 outMux7({1'b 0 , c2md[7],c2md[7] ,c2md[6] , md[6] , md[7] , md[7], 1'b0 },select,y[7] );
 
-full_adder8 alu(acc,y,z,carry);
+adder8 alu(acc,y,z,carry);
 
-assign display = {acc,mp};
+assign result = {acc,Mpand};
 endmodule
 
 
-
-
-
-
-module controller(go,flag,ld,sel,state,clk,rst);
-input go,flag,clk,rst;
+module controller(Go,flag,ld,sel,state,clk,rst);
+input Go,flag,clk,rst;
 output [4:0] ld ;
 output [4:0]sel;
 output [2:0] state;
 wire[2:0] state;
 wire [2:0] nextstate;
 
-
 register_n #(3)r1(nextstate,state,clk,rst);
 
-next_state nxt(state,flag,go,nextstate);
+next_state nxt(state,flag,Go,nextstate);
 load_sel ls(state,ld,sel);
 
 endmodule
 
 
-module radix4booth(sw,go,clk,rst,display,state);
+module radix4Booth(sw,Go,clk,rst,result,state);
 
 input [7:0] sw;
-input clk , rst,go;
-output [15:0] display;
+input clk , rst,Go;
+output [15:0] result;
 wire [4:0] ld;
 wire [4:0] sel;
 wire flag;
 output [2:0]state;
-datapath dp(ld,sel,clk,sw,rst,flag,display);
-controller cont(go,flag,ld,sel,state,clk,rst);
-
+datapath data(ld,sel,clk,sw,rst,flag,result);
+controller control(Go,flag,ld,sel,state,clk,rst);
 endmodule
 
-
-
-
-module datatest;
-
+//need to comment out testbenches for bit file generation
+/* 
+module test_datapath;
 	// Inputs
 	reg [4:0] ld;
 	reg [4:0] sel;
@@ -417,7 +367,7 @@ module datatest;
 
 	// Outputs
 	wire flag;
-	wire [15:0] display;
+	wire [15:0] result;
 
 	// Instantiate the Unit Under Test (UUT)
 	datapath uut (
@@ -427,7 +377,7 @@ module datatest;
 		.sw(sw), 
 		.rst(rst), 
 		.flag(flag), 
-		.display(display)
+		.result(result)
 	);
 
   always #10 clk = ~clk ;
@@ -454,118 +404,74 @@ module datatest;
 		sw = 5;
 		rst = 0;
       #10;
-      // Add stimulus here
-
 	end
-      
 endmodule
 
 
-module radixtest;
+module test_radix;
 
 	// Inputs
 	reg [7:0] sw;
-	reg go;
+	reg Go;
 	reg clk;
 	reg rst;
 
 	// Outputs
-	wire [15:0] display;
+	wire [15:0] result;
 	wire [2:0] state;
 	
 	// Instantiate the Unit Under Test (UUT)
-	radix4booth uut (
+	radix4Booth uut (
 		.sw(sw), 
-		.go(go), 
+		.Go(Go), 
 		.clk(clk), 
 		.rst(rst), 
-		.display(display), 
+		.result(result), 
 		.state(state)	
 	);
 
   always #10 clk = ~clk;
 	initial begin
 		// Initialize Inputs
-		sw = 68;
-		go = 1;
+		sw = 55;
+		Go = 1;
 		clk = 1;
 		rst = 1;
 
-		#10;
-      
-		sw =68;
-		go = 1;
+		#10;  
+		sw =55;
+		Go = 1;
 		rst = 1;
 
 		#10;
-		
-		sw = 68;
-		go = 1;
+		sw = 55;
+		Go = 1;
+		rst = 0;
+
+		#60;
+		sw =55;
+		Go = 0;
 		rst = 0;
 
 		#10;
-		sw = 68;
-		go = 1;
+		sw = 20;
+		Go = 0;
 		rst = 0;
 
 		#10;
-		sw = 68;
-		go = 1;
+		sw = 20;
+		Go = 1;
 		rst = 0;
 
-		#10;
-		sw = 68;
-		go = 1;
-		rst = 0;
-
-		#10;
-		sw = 68;
-		go = 1;
-		rst = 0;
-
-		#10;
-		sw = 68;
-		go = 1;
-		rst = 0;
-
-		#10;
-		
-
-		sw =68;
-		go = 0;
-		rst = 0;
-
-		#10;
-
-		sw = 35;
-		go = 0;
-		rst = 0;
-
-		#10;
-
-		sw = 35;
-		go = 1;
-		rst = 0;
-
-		#10;
-
-		sw = 35;
-		go = 1;
-		rst = 0;
-
-		#10;
-
-		// Add stimulus here
-
+		#20;
 	end
-      
 endmodule
 
 
-module cont_check;
+module test_controller;
 
 	// Inputs
-	reg go;
+	reg Go;
 	reg flag;
 	reg clk;
 	reg rst;
@@ -577,7 +483,7 @@ module cont_check;
 
 	// Instantiate the Unit Under Test (UUT)
 	controller uut (
-		.go(go), 
+		.Go(Go), 
 		.flag(flag), 
 		.ld(ld), 
 		.sel(sel), 
@@ -591,93 +497,33 @@ always #10 clk = ~clk;
 		// Initialize Inputs
 		
 	clk=0;	
-		go = 1;
+		Go = 1;
 		flag = 0;
 		rst=1;
-		#10;
-		go = 1;
-		flag = 0;
-		rst=1;
-		#10;
-		go = 1;
-		flag = 0;
-		rst=1;
-		#10;
-		go = 1;
-		flag = 0;
-		rst=1;
-		#10;
-		
-			rst=0;
-			go = 1;
-		flag = 0;
-		#10;
-			go = 1;
-		flag = 0;
-		#10;
-		go = 1;
-		flag = 0;
-		#10;
-		go = 1;
-		flag = 0;
-		#10;
-			go = 0;
-		flag = 0;
-		#10;
-			go = 0;
-		flag = 0;
-		#10;
-			go = 1;
-		flag = 0;
-		#10;	
-		go = 1;
-		flag = 0;
-		#10;
-       	go = 1;
-		flag = 0;
-		#10;	go = 1;
-		flag = 0;
-		#10;	go = 1;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10; 
-		
-			go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-			go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;	go = 0;
-		flag = 0;
-		#10;
-		#10;	go = 0;
-		flag = 1;
-		#10;	go = 0;
-		flag = 1;
-		#10;
-		// Add stimulus here
 
+		#40;
+		Go = 1;
+		flag = 0;
+		rst=0;
+		
+		#40;
+		Go = 0;
+		flag = 0;
+
+		#20;
+		Go = 1;
+		flag = 0;
+
+		#50;
+		Go = 0;
+		flag = 0;
+
+		#140;	
+		Go = 0;
+		flag = 1;
+		
+		#20;
 	end
-      
 endmodule
 
+*/
